a7ee06adbf518766bf923853c8061e4675cdf89f
gwillemssen <57201850+gwillemssen@users.noreply.github.com>
2020-09-22 20:09:57 -0500

    Redid Project
    Old project crashes Unity, this is new one

---
+++ Assets/Scripts/RigidbodyCharControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharControl : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 10;
+
+    [SerializeField]
+    private float maxSpeed = 2;
+
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicMaterial, movingPhysicMaterial;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+    private new Collider collider;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+
+        collider.material = inputDirection.magnitude > 0 ? movingPhysicMaterial : stoppingPhysicMaterial;
+
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
+        }
+
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+}
